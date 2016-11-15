6fc1aee17f338ea9fdc8981a384c3bcddff59acf
healsea <healsea1994@gmail.com>
2016-11-15 16:53:12 -0600

    real Finish of relation between stress and fstr

--- stress_fstr.m
+++ stress_fstr.m
@@ -7,11 +7,13 @@ for k = 1:length(fstrEasy)
     % overwrite original parameter
     fstr_easy = fstrEasy(k);
     stress
-    stre(k,:) = S;
+    stre(k,:) = S*0.003^2*3.1416;
     k
 end
 for i=1:10
     plot(lambda,stre(i,:))
-    legend('1','2','3','4','5','6','7','8','9','10')
+    legend('fstr = 1','fstr = 2','fstr = 3','fstr = 4','fstr = 5','fstr = 6','fstr = 7','fstr = 8','fstr = 9','fstr = 10')
     hold on
 end
+xlabel('stretch ratio')
+ylabel('force/N')
