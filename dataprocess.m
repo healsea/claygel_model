74b9a65e5b40657406bbe62603d9e82cf3053693
healsea <healsea1994@gmail.com>
2016-11-19 19:06:43 -0600

    change

--- dataprocess.m
+++ dataprocess.m
@@ -3,5 +3,5 @@ load('1117_thin5_112.mat');
 pos = pos(2:length(pos)-100);
 load = load(2:length(load)-100);
 load = medfilt1(load,15);
-lam = (pos-pos(1))/112 + 1;
+lam = (pos-pos(1))/11.2 + 1;
 plot(lam,load)
