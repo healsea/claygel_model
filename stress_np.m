69fc8e0e6c06a0bc8dfc81bc85e16ce17b13ee3a
healsea <healsea1994@gmail.com>
2016-11-20 11:02:03 -0600

    get np

--- stress_np.m
+++ stress_np.m
@@ -1,7 +1,7 @@
 % stress change with np
 p4
 
-npp = logspace(19+log10(2.3),19+log10(3),5);
+npp = logspace(18+log10(4),18+log10(5.6),5);
 stre = zeros(length(npp),length(lambda));
 for k = 1:length(npp)
     % overwrite original parameter
@@ -10,15 +10,17 @@ for k = 1:length(npp)
     stre(k,:) = S;
     k
 end
-for i=1:10
+5
+xlabel('stretch ratio')
+
+% for i=1:5:length(lambda)
+%     plot(npp,stre(:,i))
+%     %legend('np = 1e19','fstr = 3e19','fstr = 8e19','fstr = 2e20','fstr = 6e20','fstr = 2e21','fstr = 5e21','fstr = 1e22','fstr = 4e22','fstr = 1e23')
+%     hold on
+% end
+
+for i=1:5
     plot(lambda,stre(i,:))
     %legend('np = 1e19','np = 3e19','np = 8e19','np = 2e20','np = 6e20','np = 2e21','np = 5e21','np = 1e22','np = 4e22','np = 1e23')
-    hold on
-end
-xlabel('stretch ratio')
-ylabel('force/N')
-for i=1:5:length(lambda)
-    plot(npp,stre(:,i))
-    %legend('np = 1e19','fstr = 3e19','fstr = 8e19','fstr = 2e20','fstr = 6e20','fstr = 2e21','fstr = 5e21','fstr = 1e22','fstr = 4e22','fstr = 1e23')
     hold on
 end
