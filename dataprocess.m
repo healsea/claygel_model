d0d432ad901fef809dc83eae8e456722bcd2c5e5
healsea <healsea1994@gmail.com>
2016-11-21 11:01:55 -0600

    CORRECT

--- dataprocess.m
+++ dataprocess.m
@@ -6,7 +6,7 @@ A = 3.1416*r^2;
 pos = pos(2:length(pos)-100);
 loadd = loadd(2:length(loadd)-100)/A;
 loadd = medfilt1(loadd,15);
-lam = (pos-pos(1))/len*1.4 + 1;
+lam = (pos-pos(1))/len + 1;
 plot(lam,loadd)
 %legend(strcat('Length = ',num2str(len),'  Radius = ',num2str(r)))
 hold on
